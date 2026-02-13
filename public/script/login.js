4e089c1595bca4afd7242f20ac3d57111aa070ee
Destroyerofdarkness <dawidadogier@gmail.com>
2026-02-13 13:40:05 +0100

    Made the user sign in fetch

---
+++ public/script/login.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = form.username.value;
+  const passwd = form.password.value;
+
+  const res = await fetch("http://10.12.18.201:6001/user/signIn", {
+    method: "POST",
+    body: JSON.stringify({ name, passwd }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+  if(data.err){
+    console.log(data.err)
+  }
+  if (data.token) {
+    window.location.href = "/";
+  }
+});
