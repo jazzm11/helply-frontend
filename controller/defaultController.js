2e37ac550f67baed7eb27570961c0de14f7d9992
Jazz Maquiraya <jazzm11>
2026-03-06 14:11:44 +0100

    jeg lagde en side for å rendere en ticket, men trenger stylingen

--- controller/defaultController.js
+++ controller/defaultController.js
@@ -5,23 +5,22 @@ const homePage = async (req, res) => {
   try {
     const response = await getApiData('/sixLatest');
     res.render('index', { data: response.Tickets || [] });
-    console.log('Fetched tickets to HOME PAGE');
+    console.log('Fetched tickets to HOME PAGE', response.Tickets);
   } catch (error) {
     console.error('Error fetching tickets:', error);
     res.status(500).render('error', { message: 'Failed to fetch tickets' });
   }
 };
 
-// User Controllers
-
-
-
 const view = async (req, res) => {
   try {
-    const response = await getApiData('/sixLatest');
-    res.render('view', { posts: response.Tickets || [] });
+    const { id } = req.params;
+    const response = await getApiData(`/view/${id}`);
+    
+    res.render('view', { post: response.userTickets, comments: null });
   } catch (err) {
     console.log(err);
+    res.status(500).render('error', { message: 'Failed to fetch ticket' });
   }
 };
 
